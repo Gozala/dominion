d482329c992c59ca7281ba6525b409bbc6c4d709
Irakli Gozalishvili <rfobic@gmail.com>
2017-09-26 15:39:52 -0700

    Add tests for styles.

--- test/DOM.js
+++ test/DOM.js
@@ -112,4 +112,121 @@ test("div with properties", async test => {
     ],
     "set / remove attribute and assign property"
   )
+
+  const v5 = DOM.createHost(
+    [],
+    [
+      DOM.createElement("div", [
+        DOM.setAttribute("e-text", "custom"),
+        DOM.property("otherProp", true)
+      ])
+    ]
+  )
+
+  test.deepEqual(
+    diff(v4, v5),
+    [
+      "selectChildren()",
+      "selectSibling(1)",
+      'assignProperty("otherProp", true)'
+    ],
+    "update property"
+  )
+})
+
+test("div with style rules", async test => {
+  const v1 = DOM.createHost()
+  const v2 = DOM.createHost(
+    [],
+    [
+      DOM.createElement(
+        "div",
+        [
+          DOM.style({
+            backgroundColor: "red"
+          })
+        ],
+        []
+      )
+    ]
+  )
+
+  test.deepEqual(
+    diff(v1, v2),
+    [
+      "selectChildren()",
+      'insertElement("div")',
+      "selectSibling(1)",
+      'setStyleRule("backgroundColor", "red")'
+    ],
+    "add div with style"
+  )
+
+  const v3 = DOM.createHost(
+    [],
+    [
+      DOM.createElement("div", [
+        DOM.style({
+          color: "white"
+        })
+      ])
+    ]
+  )
+
+  test.deepEqual(
+    diff(v2, v3),
+    [
+      "selectChildren()",
+      "selectSibling(1)",
+      'removeStyleRule("backgroundColor")',
+      'setStyleRule("color", "white")'
+    ],
+    "set / remove style rule"
+  )
+
+  const v4 = DOM.createHost(
+    [],
+    [
+      DOM.createElement("div", [
+        DOM.style({
+          color: null,
+          backgroundColor: "blue"
+        })
+      ])
+    ]
+  )
+
+  test.deepEqual(
+    diff(v3, v4),
+    [
+      "selectChildren()",
+      "selectSibling(1)",
+      'removeStyleRule("color")',
+      'setStyleRule("backgroundColor", "blue")'
+    ],
+    "set / remove style rules"
+  )
+
+  const v5 = DOM.createHost(
+    [],
+    [
+      DOM.createElement("div", [
+        DOM.style({
+          color: "white",
+          backgroundColor: "green"
+        })
+      ])
+    ]
+  )
+
+  test.deepEqual(
+    diff(v4, v5),
+    [
+      "selectChildren()",
+      "selectSibling(1)",
+      'setStyleRule("color", "white")',
+      'setStyleRule("backgroundColor", "green")'
+    ],
+    "update / add style rule"
+  )
 })
