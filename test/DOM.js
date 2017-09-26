575eca4d62ce12098955bc825f7e3e15989e3ae9
Irakli Gozalishvili <rfobic@gmail.com>
2017-09-26 15:24:05 -0700

    Write some basic tests.

--- test/DOM.js
+++ test/DOM.js
@@ -1,8 +1,115 @@
 /* @flow */
 
 import * as DOM from "../"
+import { createHost, diff } from "./Util"
 import test from "blue-tape"
 
-test("test baisc", async test => {
-  test.isEqual(typeof DOM, "object")
+test("empty div", async test => {
+  const v1 = DOM.createHost()
+  const v2 = DOM.createHost([], [DOM.createElement("div")])
+
+  test.deepEqual(
+    diff(v1, v2),
+    ["selectChildren()", 'insertElement("div")'],
+    "insert div"
+  )
+})
+
+test("div with attributes", async test => {
+  const v1 = DOM.createHost()
+  const v2 = DOM.createHost([], [DOM.createElement("div")])
+  const v3 = DOM.createHost(
+    [],
+    [
+      DOM.createElement("div", [
+        DOM.setAttribute("class", "standard"),
+        DOM.setAttribute("e-text", "custom")
+      ])
+    ]
+  )
+
+  test.deepEqual(
+    diff(v2, v3),
+    [
+      "selectChildren()",
+      "selectSibling(1)",
+      'setAttribute("class", "standard")',
+      'setAttribute("e-text", "custom")'
+    ],
+    "add two attributes to an empty div"
+  )
+
+  test.deepEqual(
+    diff(v1, v3),
+    [
+      "selectChildren()",
+      'insertElement("div")',
+      "selectSibling(1)",
+      'setAttribute("class", "standard")',
+      'setAttribute("e-text", "custom")'
+    ],
+    "insert div with two attributes"
+  )
+})
+
+test("div with properties", async test => {
+  const v1 = DOM.createHost()
+  const v2 = DOM.createHost(
+    [],
+    [DOM.createElement("div", [DOM.setAttribute("class", "standard")], [])]
+  )
+
+  test.deepEqual(
+    diff(v1, v2),
+    [
+      "selectChildren()",
+      'insertElement("div")',
+      "selectSibling(1)",
+      'setAttribute("class", "standard")'
+    ],
+    "add div with attribute"
+  )
+
+  const v3 = DOM.createHost(
+    [],
+    [
+      DOM.createElement("div", [
+        DOM.setAttribute("e-text", "custom"),
+        DOM.property("testProp", 5)
+      ])
+    ]
+  )
+
+  test.deepEqual(
+    diff(v2, v3),
+    [
+      "selectChildren()",
+      "selectSibling(1)",
+      'assignProperty("testProp", 5)',
+      'removeAttribute("class")',
+      'setAttribute("e-text", "custom")'
+    ],
+    "set / remove attribute and assign property"
+  )
+
+  const v4 = DOM.createHost(
+    [],
+    [
+      DOM.createElement("div", [
+        DOM.setAttribute("e-text", "custom"),
+        DOM.property("otherProp", null)
+      ])
+    ]
+  )
+
+  test.deepEqual(
+    diff(v3, v4),
+    [
+      "selectChildren()",
+      "selectSibling(1)",
+      'deleteProperty("testProp")',
+      'assignProperty("otherProp", null)'
+    ],
+    "set / remove attribute and assign property"
+  )
 })
---
+++ test/Util.js
@@ -0,0 +1,24 @@
+/* @flow */
+
+import * as DOM from "../"
+import * as FlatBuffer from "../lib/Log/FlatBuffer"
+import { patcher } from "../lib/Patch/Log"
+import { JSDOM } from "jsdom"
+
+export const diff = <a>(
+  left: DOM.Node<a>,
+  right: DOM.Node<a>
+): Array<string> => {
+  const host = createHost("main")
+  const encoder = FlatBuffer.encoder()
+
+  const delta = DOM.diff(left, right, encoder)
+  const buffer = encoder.encode()
+  const decoder = FlatBuffer.decoder(buffer)
+  const logger = patcher()
+  DOM.patch(host, decoder, logger)
+  return logger.log
+}
+
+export const createHost = (localName: string = "main"): HTMLElement =>
+  new JSDOM().window.document.createElement(localName)
