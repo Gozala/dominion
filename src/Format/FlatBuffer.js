526d1c85b9e74376d389824220d1670bbee12ce5
Irakli Gozalishvili <rfobic@gmail.com>
2017-10-06 13:57:09 -0700

    Expose encoder function from FlatBuffer module.

--- src/Format/FlatBuffer.js
+++ src/Format/FlatBuffer.js
@@ -6,6 +6,7 @@ import Decoder from "./FlatBuffer/Decoder"
 export default class FlatBuffer {
   static Encoder = Encoder
   static Decoder = Decoder
+  static encoder = Encoder.encoder
   static encode = Encoder.encode
   static decode = Decoder.decode
 }
